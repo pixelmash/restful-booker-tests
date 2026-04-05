777d806aaeaaf3159a1400a409a4a65bef34e518
pixelmash <239129817+pixelmash@users.noreply.github.com>
2026-04-05 22:26:44 +0200

    Add additional rooms API interception tests

--- tests/api/interception.spec.ts
+++ tests/api/interception.spec.ts
@@ -1,6 +1,34 @@
 import { test, expect } from '@playwright/test';
+import { testData } from '../../testData';
 
-test('shows 10 rooms when API returns 2 rooms', async ({ page }) => {
+test.fail('shows info message when when API returns no rooms', async ({ page }) => {
+    let rooms = Array();
+    await page.route(testData.urls.ui + '/api/room', async route => {
+        await route.fulfill({
+            status: 200,
+            contentType: 'application/json',
+            body: JSON.stringify({ rooms: rooms })
+        });
+    });
+    await page.goto(testData.urls.ui as string, { waitUntil: 'networkidle' });
+    await expect(page.locator('.room-card')).toHaveCount(0);
+    // No info message pops-up at the moment
+    await expect(page.getByText("Sorry, there're no available rooms at the moment")).toBeVisible();
+});
+
+test.fail('shows info message when when API returns error', async ({ page }) => {
+    await page.route(testData.urls.ui + '/api/room', async route => {
+        const response = await route.fetch();
+        const body = await response.json();
+        await route.fulfill({ status: 500, body: JSON.stringify(body) });
+    });
+    await page.goto(testData.urls.ui as string, { waitUntil: 'networkidle' });
+    // No error handler. Page just doesn't load on failed API request
+    await expect(page.locator('.room-card')).toHaveCount(0);
+});
+
+
+test('shows 2 rooms when API returns 2 rooms', async ({ page }) => {
     let rooms = Array();
     for (let i = 0; i < 2; i++) {
         rooms[i] = {
@@ -17,15 +45,13 @@ test('shows 10 rooms when API returns 2 rooms', async ({ page }) => {
             "roomPrice": i * 100
         }
     }
-    await page.route('https://automationintesting.online/api/room', async route => {
+    await page.route(testData.urls.ui + '/api/room', async route => {
         await route.fulfill({
             status: 200,
             contentType: 'application/json',
             body: JSON.stringify({ rooms: rooms })
         });
     });
-
-    await page.goto('https://automationintesting.online', { waitUntil: 'networkidle' });
-
+    await page.goto(testData.urls.ui as string, { waitUntil: 'networkidle' });
     await expect(page.locator('.room-card')).toHaveCount(2);
 });
